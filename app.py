bdf4e9658f5119a8a2bcf6decfd8f81e4f0514f8
Vaibhav <vaibhavyangkai123@gmail.com>
2023-12-11 19:10:07 +0530

    update prediction service

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import os
 import yaml
 import joblib
 import numpy as np 
+from prediction_service import prediction
 
-params_path = "params.yaml"
+# params_path = "params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root,"static")
@@ -12,46 +13,23 @@ templates_dir = os.path.join(webapp_root,"templates")
 
 app = Flask(__name__,static_folder=static_dir,template_folder=templates_dir)
 
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response":response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error":"something went wrong "}
-        return error
-    
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
-
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float,data))]
-                response = predict(data)
+                data_req = dict(request.form)
+                # data = [list(map(float,data))]
+                response = prediction.form_response(data_req)
                 return render_template("index.html",response=response)
             elif request.json:
-                reponse = api_response(request)
+                reponse = prediction.api_response(request.json)
                 return jsonify(response) 
+            
         except Exception as e:
             print(e)
-            error = {"error":"Something went wrong !! So fuck off"}
-            return render_template("404.html",error=error)
+            # error = {"error":"Something went wrong !! So fuck off"}
+            return render_template("404.html",error=e)
     else:
         return render_template("index.html")
 
